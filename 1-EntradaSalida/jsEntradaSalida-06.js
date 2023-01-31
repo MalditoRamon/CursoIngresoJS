f45fe46be676d8d81e367f99e4a51593a91d45ff
Lucas Ramón <malditoprogramador@hotmail.com>
2023-01-30 21:20:14 -1100

    hecho

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,8 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let n1 = document.getElementById("txtIdNumeroUno").value;
+	let n2 = document.getElementById("txtIdNumeroDos").value;
+	alert(parseInt(n1) + parseInt(n2));
 }
 
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -3,7 +3,9 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-function SacarResto()
-{
-	alert("ok");
+function SacarResto() {
+	let n1 = document.getElementById("txtIdNumeroDividendo").value
+	let n2 = document.getElementById("txtIdNumeroDivisor").value
+	let resultado = parseInt(n1) % parseInt(n2)
+	alert(" el resto es " + resultado);
 }
