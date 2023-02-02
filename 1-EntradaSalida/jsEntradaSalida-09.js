301b3ee5b605ae99e45ab09f6e4da8106fedbd2d
Lucas Ramón <malditoprogramador@hotmail.com>
2023-02-01 19:46:10 -1100

    hecho

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let importe = parseInt(document.getElementById("txtIdSueldo").value)
+	let resultado = document.getElementById("txtIdResultado") 
+	
+	resultado.value = importe * 10 / 100 + importe
 
-	alert("ok");
+	
 }
--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,23 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    let precioUno = document.getElementById("txtIdPrecioUno").value 
+    let precioDos = document.getElementById("txtIdPrecioDos").value
+    let precioTres = document.getElementById("txtIdPrecioTres").value
+    alert(parseInt(precioUno) + parseInt(precioDos) + parseInt (precioTres))
+
 }
 function Promedio () 
 {
-	
+    let precioUno = document.getElementById("txtIdPrecioUno").value
+    let precioDos = document.getElementById("txtIdPrecioDos").value
+    let precioTres = document.getElementById("txtIdPrecioTres").value
+    alert( (parseInt(precioUno) + parseInt(precioDos) + parseInt(precioTres)) / 3)
 }
 function PrecioFinal () 
 {
-	
+	let precioUno = parseInt(document.getElementById("txtIdPrecioUno").value)
+	let precioDos = parseInt(document.getElementById("txtIdPrecioDos").value)
+	let precioTres = parseInt(document.getElementById("txtIdPrecioTres").value)
+    alert((precioUno + precioDos + precioTres) * 21 / 100 + precioUno + precioDos + precioTres)
 }
